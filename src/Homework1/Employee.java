fde0bd8855751f93bdc5422dde068259bad37475
sers1 <sers156@mail.ru>
2023-11-29 15:36:23 +1000

    hw9

--- src/Homework1/Employee.java
+++ src/Homework1/Employee.java
@@ -1,4 +1,41 @@
 package Homework1;
 
 public class Employee {
+    int id;
+    public String surname;
+    int age;
+    private double salary;
+    String department;
+
+    public Employee(int id2) {
+        id = id2;
+
+    }
+
+    Employee(String surname2) {
+        surname = surname2;
+
+    }
+
+    private Employee(double salary2) {
+        salary = salary2;
+
+    }
+
+    double uvelichenieSalary() {
+        salary *= 2;
+        return salary;
+
+    }
+
+    public void Id() {
+        System.out.println("ID = " + id);
+    }
+    public void Surname() {
+        System.out.println("Surname = " + surname);
+    }
+    public void Salary() {
+        System.out.println("Salary = " + salary);
+    }
 }
+
